c1cd13ac3ba6009bd6a0436229e451f9bd54bf40
peter-siebold <peter.siebold@icloud.com>
2018-03-05 06:28:12 +0100

    added general layout for recipe list and shopping list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,10 @@ export class RecipeListComponent implements OnInit {
     new Recipe( 'Adobo',
                 'Adobo Dry Rub Seasoning',
     'https://fthmb.tqn.com/OeqsXfD6YFQ8qecHJ4IiEo3cRhU=/1280x860/filters:no_upscale()/11618850496_977c32d407_o-56a58b1c5f9b58b7d0dd4f8e.jpg'
-              )
+              ),
+    new Recipe('Pad Kra Prao', 'The famous fried basil with chicken dish',
+    'https://migrationology.smugmug.com/Thai-Recipes/i-S5pmbsQ/0/X3/thai-chicken-basil-recipe-4-X3.jpg')
+
   ];
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Spring Onions', 6),
+    new Ingredient('Chili', 4),
+    new Ingredient('Chicken Breast', 2)
+  ];
   constructor() { }
 
   ngOnInit() {
