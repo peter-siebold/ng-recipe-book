2d00cb2200e5aa0eeb73e3e809a0cae5248ef46a
peter-himself <peter.siebold@icloud.com>
2018-03-06 16:20:50 +0100

    added recipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,22 +8,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe( 'Adobo',
-                'Adobo Dry Rub Seasoning',
-    'https://fthmb.tqn.com/OeqsXfD6YFQ8qecHJ4IiEo3cRhU=/1280x860/filters:no_upscale()/11618850496_977c32d407_o-56a58b1c5f9b58b7d0dd4f8e.jpg'
-              ),
-    new Recipe('Pad Kra Prao', 'The famous fried basil with chicken dish',
-    'https://migrationology.smugmug.com/Thai-Recipes/i-S5pmbsQ/0/X3/thai-chicken-basil-recipe-4-X3.jpg')
-
-  ];
-  @Output() recipeWasSelected = new EventEmitter<void>();
-  constructor() {
+  recipes: Recipe[];
+  constructor(private recipeService: RecipeService) {
   }
- ngOnInit() { }
-  onRecipeSelected(recipe) {
-    console.log('onRecipeSelected');
-    this.recipeWasSelected.emit(recipe);
-  }
+ ngOnInit() {
+   this.recipes = this.recipeService.getRecipes();
+ }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,18 @@
+import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe( 'Adobo',
+                'Adobo Dry Rub Seasoning',
+    'https://fthmb.tqn.com/OeqsXfD6YFQ8qecHJ4IiEo3cRhU=/1280x860/filters:no_upscale()/11618850496_977c32d407_o-56a58b1c5f9b58b7d0dd4f8e.jpg'
+              ),
+    new Recipe('Pad Kra Prao', 'The famous fried basil with chicken dish',
+    'https://migrationology.smugmug.com/Thai-Recipes/i-S5pmbsQ/0/X3/thai-chicken-basil-recipe-4-X3.jpg')
 
+  ];
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
