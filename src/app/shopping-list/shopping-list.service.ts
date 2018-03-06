c8c6d04c83fb3c9fdfab5deae2919e82fb6a5f98
peter-himself <peter.siebold@icloud.com>
2018-03-06 17:13:43 +0100

    shoppinglist

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
-export class ShoppingListService {
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Spring Onions', 6),
+    new Ingredient('Chili', 4),
+    new Ingredient('Chicken Breast', 2)
+  ];
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
